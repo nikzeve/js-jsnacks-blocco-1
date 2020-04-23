261b7d1ef39e4dbe587503a8f0e92b80c931c5e6
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-04-23 18:01:37 +0200

    push intermedia

---
+++ jsnack5/main.js
@@ -0,0 +1,14 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente
+
+// var cubo_di_quattro = Math.pow(4, 3);
+// console.log(cubo_di_quattro);
+
+
+var n_utente = prompt('Inserisci fino a che numero vuoi che venga eseguito il cubo.')
+console.log(n_utente);
+
+
+for (var i = 1; i <= n_utente; i++) {
+    var cubo = Math.pow(i, 3);
+    console.log(cubo + ' :cubo di ' + i);
+}
